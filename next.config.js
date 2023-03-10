2ee38049de99ff47cf151ee21f683ea8855a17f7
JeongjooLee <jjlee1623@gmail.com>
2023-03-10 17:01:44 +0900

    remove : rewrite

--- next.config.js
+++ next.config.js
@@ -10,14 +10,6 @@ const nextConfig = {
   compiler: {
     emotion: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/holiday",
-        destination: `https://www.googleapis.com/calendar/v3/calendars/ko.south_korea%23holiday%40group.v.calendar.google.com/events?key=${GOOGLE_API_KEY}&orderBy=startTime&singleEvents=true&timeMin=1990-01-01T00:00:00Z&timeMax=2041-01-01T00:00:00Z`,
-      },
-    ];
-  },
 };
 
 module.exports = nextConfig;
