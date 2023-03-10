bbc5bb95f8249dfcf1b948fefaf035a371aa6a64
JeongjooLee <jjlee1623@gmail.com>
2023-03-10 20:43:49 +0900

    feat : ga 조회수 이벤트 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,18 +5,35 @@ import { ChakraBaseProvider, extendBaseTheme } from "@chakra-ui/react";
 import chakraTheme from "@chakra-ui/theme";
 import styled from "@emotion/styled";
 import Script from "next/script";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import * as gtag from "@/lib/gtag";
 
 const theme = extendBaseTheme({
   components: chakraTheme.components,
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
       {/* Google Analytics */}
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
@@ -27,7 +44,7 @@ export default function App({ Component, pageProps }: AppProps) {
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
         
-          gtag('config', ${process.env.GA_TRACKING_ID});
+          gtag('config', ${gtag.GA_TRACKING_ID});
           `,
         }}
       />
