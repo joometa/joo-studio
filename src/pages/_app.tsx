a014b69a5499d684c62255a274d330c88a1a81d2
JeongjooLee <jjlee1623@gmail.com>
2023-03-14 00:17:57 +0900

    feat : gtm setting

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,8 +6,8 @@ import chakraTheme from "@chakra-ui/theme";
 import styled from "@emotion/styled";
 import Script from "next/script";
 import { useRouter } from "next/router";
+import { GTM_ID, pageview } from "../lib/gtm";
 import { useEffect } from "react";
-import * as gtag from "@/lib/gtag";
 
 const theme = extendBaseTheme({
   components: chakraTheme.components,
@@ -17,38 +17,28 @@ export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
   useEffect(() => {
-    const handleRouteChange = (url: any) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    router.events.on("hashChangeComplete", handleRouteChange);
+    router.events.on("routeChangeComplete", pageview);
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-      router.events.off("hashChangeComplete", handleRouteChange);
+      router.events.off("routeChangeComplete", pageview);
     };
   }, [router.events]);
 
   return (
     <>
-      {/* Google Analytics */}
+      {/* Google Tag Manager - Global base code */}
       <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
+        id="gtag-base"
         strategy="afterInteractive"
         dangerouslySetInnerHTML={{
           __html: `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-        
-          gtag('config', ${gtag.GA_TRACKING_ID});
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
           `,
         }}
       />
-
       <ChakraBaseProvider theme={theme}>
         <Header />
         <MainContainer>
